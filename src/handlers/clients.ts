2a197dcbc1ec9288dbae72a66d2c7959d50d232f
Benjamin-ochieng <ochiengbenja@gmail.com>
2023-02-24 14:16:47 +0000

    add handlers

--- src/handlers/clients.ts
+++ src/handlers/clients.ts
@@ -1,12 +1,62 @@
 import prisma from "../db";
 
 export const createClient = async (req, res) => {
-  const newClient = await prisma.client.create({
-    data: {
-      name: req.body.name,
-      email: req.body.email,
+   try {
+      const newClient = await prisma.client.create({
+        data: {
+          clientName: req.body.clientName,
+          clientEmail: req.body.clientEmail,
+          userId: req.user.id,
+        },
+      });
+
+      return res.status(201).json({ data: newClient });
+   } catch (error) {
+    console.log(error);
+    
+   }
+};
+
+export const getManyClients = async (req, res) => {
+  const clients = await prisma.client.findMany({
+    where: {
+      userId: req.user.id,
     },
   });
+  return res.status(200).json({ data: clients });
+};
+export const getOneClient = async (req, res) => {
+  const client = await prisma.client.findUnique({
+    where: {
+      id_userId: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+    },
+  });
+  return res.status(200).json({ data: client });
+};
+export const updateOneClient = async (req, res) => {
+    const updatedClient = await prisma.client.update({
+    where: {
+      id_userId: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+    },
+    data: req.body
+  });
+  return res.status(200).json({ data: updatedClient });
+};
+export const deleteOneClient = async (req, res) => {
+      const deletedClient = await prisma.client.delete({
+    where: {
+      id_userId: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+    },
 
-  return res.status(200).json({ data: newClient });
+  });
+  return res.status(200).json({ data: deletedClient });
 };
--- src/handlers/invoices.ts
+++ src/handlers/invoices.ts
@@ -1,21 +1,68 @@
-import prisma from '../db'
+import prisma from "../db";
 
 export const createInvoice = async (req, res) => {
-    const newInvoice = await prisma.invoice.create({
-      data: {
-        title: req.body.title,
-        total: req.body.total,
-        clientId: req.body.clientId,
-        dueDate: req.body.dueDate,
-      },
-    });
-
-    return res.status(200).json({data: newInvoice})
- };
-export const getManyInvoices = (req, res) => {
-    res.status(200).json({ message: "Testing the invoices router"})
+  const authorizedUser = await prisma.user.findFirst({
+    where: {
+      id: req.user.id,
+      clients: { some: { id: req.body.clientId } },
+    },
+  });
+  if (!authorizedUser) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  const { invoiceAmount, invoiceTitle, clientId } = req.body;
+  const invoice = await prisma.invoice.create({
+    data: {
+      invoiceAmount,
+      invoiceTitle,
+      clientId,
+      userId: req.user.id,
+    },
+  });
+  res.status(201).json({ date: invoice });
 };
-export const getOneInvoice = () => {};
-export const updateInvoice = () => { };
-export const deleteInvoice = () => {};
-
+export const getManyInvoices = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: req.user.id },
+    select: { invoices: true },
+  });
+  const { invoices } = user;
+  res.status(200).json({ data: invoices });
+};
+export const getOneInvoice = async (req, res) => {
+  const invoice = await prisma.invoice.findUnique({
+    where: {
+      id_userId: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+    },
+  });
+  if (!invoice) {
+    return res.status(404).json({ error: "Invoice not found" });
+  }
+  res.status(200).json({ data: invoice });
+};
+export const updateInvoice = async (req, res) => {
+  const updatedInvoice = await prisma.invoice.update({
+    where: {
+      id_userId: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+      },
+      data: req.body
+  });
+    res.status(200).json({ data: updatedInvoice });
+};
+export const deleteInvoice = async (req, res) => {
+    const deletedInvoice = await prisma.invoice.delete({
+        where: {
+        id_userId: {
+            id: req.params.id,
+            userId: req.user.id,
+        },
+        },
+    });
+    res.status(200).json({ data: deletedInvoice });
+};
