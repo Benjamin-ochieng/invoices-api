3891b92685c44066f473c488a82eb3e03aee3ec3
Benjamin-ochieng <ochiengbenja@gmail.com>
2023-02-24 14:27:31 +0000

    auth updates

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -28,40 +28,40 @@ export const verifyToken = (token) =>
   });
 
 
- export const signup = async (req, res, next) => {
-   try {
-     const hash = await hashPassword(req.body.password);
-     const newUser = await prisma.user.create({
-       data: {
-         userName: req.body.userName,
-         password: hash,
-       },
-     });
-     const token = createJWT(newUser);
-     res.json({ token });
-   } catch (err) {
-     err.type = "input";
-     next(err);
-   }
- };
+export const signup = async (req, res, next) => {
+  try {
+    const hash = await hashPassword(req.body.password);
+    const newUser = await prisma.user.create({
+      data: {
+        userName: req.body.userName,
+        password: hash,
+      },
+    });
+    const token = createJWT(newUser);
+    res.json({ token });
+  } catch (err) {
+    err.type = "input";
+    next(err);
+  }
+};
 
- export const signin = async (req, res) => {
-   const user = await prisma.user.findUnique({
-     where: {
-       userName: req.body.userName,
-     },
-   });
+export const signin = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      userName: req.body.userName,
+    },
+  });
 
-   let invalid = { message: "Incorrect email password combination" };
+  let invalid = { message: "Incorrect email password combination" };
 
-   const isValidUser = await comparePassword(req.body.password, user.password);
-   if (!isValidUser) {
-     res.status(401).send(invalid);
-   } else {
-     const token = createJWT(user);
-     res.json({ token });
-   }
- }; 
+  const isValidUser = await comparePassword(req.body.password, user.password);
+  if (!isValidUser) {
+    res.status(401).send(invalid);
+  } else {
+    const token = createJWT(user);
+    res.json({ token });
+  }
+};
 
 export const protect = async (req, res, next) => {
   const bearer = req.headers.authorization;
