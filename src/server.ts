d5fa8d4710139847ab4eab5335d4f38053a56877
Benjamin-ochieng <ochiengbenja@gmail.com>
2023-03-14 19:12:57 +0000

    feat: setup config files

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,30 @@
-import express from 'express'
-import router from './router'
-import cors from 'cors'
-import morgan from 'morgan'
-import { signup, signin, protect } from './modules/auth'
-import { handleInputErrors, validateInputs } from './modules/middleware'
+import express from "express";
+import router from "./router";
+import cors from "cors";
+import morgan from "morgan";
+import { signup, signin, protect } from "./modules/auth";
+import { handleInputErrors, validateInputs } from "./modules/middleware";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(morgan("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const operationalErrorHandler = (err, req, res, next) => {  
+  if (err.isOperational) {
+    res.status(err.httpCode).json({ error: err });
+  } else next(err);
+};
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({message: "Hello World!"});
+  res.json({ message: "Hello World!" });
 });
 
 app.use("/signup", [validateInputs("signup"), handleInputErrors], signup);
 app.use("/signin", [validateInputs("signin"), handleInputErrors], signin);
-app.use('/api', protect, router)
+app.use("/api", protect, router);
+app.use(operationalErrorHandler);
 
-export default app
+export default app;
